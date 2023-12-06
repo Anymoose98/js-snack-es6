2afd968c696c8cf1531a9ee8e8263ff63f2397d8
Anymoose98 <daniel.digiacomo98@gmail.com>
2023-12-06 19:57:15 +0100

    Creazione squadre di calcio

---
+++ SNACK2/js/script.js
@@ -0,0 +1,19 @@
+let squadreDiCalcio = [
+    {
+        nome: "Roma",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Lazio",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Milan",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+];
+
+console.log(squadreDiCalcio);
