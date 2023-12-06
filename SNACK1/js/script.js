53aacd559f894aa90c655d3e6bfee3fc7aeaaa86
Anymoose98 <daniel.digiacomo98@gmail.com>
2023-12-06 18:37:59 +0100

    Creato array e sistemato collegamento con js

---
+++ SNACK1/js/script.js
@@ -0,0 +1,25 @@
+let bici_da_corsa = [
+    {
+        nome : "Trek Madone SLR 9 Disc",
+        peso : "8",
+    },
+    {
+        nome : "Specialized S-Works Tarmac",
+        peso : "7",
+    },
+    {
+        nome : "Canyon Ultimate CF SLX:",
+        peso : "7",
+    },
+    {
+        nome : "Pinarello Dogma F12",
+        peso : "6",
+    },
+    {
+        nome : "Cervélo R5",
+        peso : "8",
+    },
+];
+
+console.log(bici_da_corsa);
+
